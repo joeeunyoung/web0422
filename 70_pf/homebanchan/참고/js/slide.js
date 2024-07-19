3354d9cb16721d57f7c053a09f849c55e29a47c6
angel144000 <angel144000@naver.com>
2024-07-19 18:06:36 +0900

    0719

--- 70_pf/homebanchan/참고/js/slide.js
+++ 70_pf/homebanchan/참고/js/slide.js
@@ -1,24 +1,10 @@
 // section1 - carousel
-// bxSlide.js Plug-in
+// jQuery bxSlide.js Plug-in
 
-/* 
-    프레임워크는 애플리케이션의 구조 또는 틀을 제공하며, 개발을 위한 기본적인 형태와 필수 요소를 포함하고 있습니다. 프레임워크가 정한 규칙과 구조 내에서 개발을 진행해야 합니다.
-    예) 부트스트랩 프레임워크
+// 문서가 준비되면;
+$(function () {
 
-    라이브러리는 특정 기능을 수행하는 코드의 집합으로, 개발자가 필요에 따라 선택적으로 사용할 수 있습니다. 예) 제이쿼리
-
-    플러그인은 어떤 특정한 하나의 문제를 해결하기 위한 컴포넌트(component)이다. 예) bx슬라이더
-*/
-
-// 바닐라 스크립트
-/* window.addEventListener("DOMContentLoaded", function(){
-    // 문서가 준비되면 실행할 코드;
-}); */
-// 제이쿼리
-$(document).ready(function () {
-    // 문서가 준비되면 실행할 코드;
-
-    $('.slide').bxSlider({
+    $('.slider').bxSlider({
 
         ////////////////////////////////////////////////////////
 
@@ -49,20 +35,21 @@ $(document).ready(function () {
 
         ////////////////////////////////////////////////////////
 
-        // auto: true(기본값) -> 자동 전환
+        // auto: false(기본값), true이면 자동 전환
         auto: true,
         // 클릭시 자동 슬라이드 정지
-        stopAutoOnClick: true,
+        //stopAutoOnClick: false(기본값)
+        //stopAutoOnClick: true,
         // pause: 4000 (기본값) -> 일시정지 시간
         //pause: 1000,
         // autoStart: true(기본값) -> 자동 슬라이드
-        autoStart: true,
+        //autoStart: true,
         // autoDirection: 'next'(기본값) -> 슬라이드 방향
         // next이면 오른쪽에서 왼쪽 방향
         // prev이면 왼쪽에서 오른쪽 방향
         //autoDirection: 'prev',
         // autoHover: false(기본값), true이면 슬라이드에 호버시 일시정지
-        autoHover: true,
+        //autoHover: true,
         // autoDelay: 0(기본값) -> 자동 슬라이드 시작 시 지연 시간
         //autoDelay: 4000
         // 슬라이드 크기(단, px 단위 생략)
